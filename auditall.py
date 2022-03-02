0c8694f4cacdcf03378bc96408b4519c008fe942
jonterola <jonterola@gmail.com>
2022-03-02 18:13:23 +0100

    Estructuración del proyecto

---
+++ auditall.py
@@ -0,0 +1,11 @@
+from auditall.attacks.email import email 
+from auditall.attacks.navigation import in_navigation,out_navigation
+from auditall.attacks.endpoint import endpoint
+
+def run() :
+    email
+    in_navigation
+    out_navigation
+    endpoint
+
+    
